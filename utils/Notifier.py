54742073f6a9b3d4055a5af82f31017c26fb3da1
Tim <herew26@gmail.com>
2022-02-10 17:16:47 +0300

    Notifier added

---
+++ utils/Notifier.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from aiogram import Dispatcher
+
+
+class Notifier:
+    def __init__(self, dp: Dispatcher, path_to_input_file: str, users_to_notify: List[str]):
+        self._dp = dp
+        self._file = path_to_input_file
+        self._users = users_to_notify
+
+    def _read_file(self) -> str:
+        try:
+            with open(self._file, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            self._clear_file()
+
+    def _clear_file(self):
+        with open(self._file, 'w') as file:
+            file.write("")
+
+    async def check(self):
+        message = self._read_file()
+        if not message:
+            return
+
+        for user in self._users:
+            await self._dp.bot.send_message(user, message)
+
+        self._clear_file()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from . import db_api
 from . import misc
+from . import Notifier
 from .notify_admins import on_startup_notify
