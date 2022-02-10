aa9c65c919105bb546ece71d2a08b928ae7f2457
Tim <herew26@gmail.com>
2022-02-10 17:17:10 +0300

    start command updated

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,15 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
+from data.config import USERS_TO_NOTIFY
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    if message.chat.id in USERS_TO_NOTIFY:
+        text = "Привет, я буду отправлять уведомления, когда цена в магазинах будет ниже чем МРЦ"
+    else:
+        text = "Привет, если хочешь получать уведомления, напиши об этом @TimNekk"
+
+    await message.answer(text)
