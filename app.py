fde5481d97643903ae297c8e2f4fec657d505764
Tim <herew26@gmail.com>
2022-02-10 17:17:18 +0300

    schedule added

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import asyncio
+
+import aioschedule
 from aiogram import executor
 
-from loader import dp
+from loader import dp, notifier
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 
+async def scheduler():
+    aioschedule.every().minute.do(notifier.check)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
 async def on_startup(dispatcher):
+    # Create Scheduler
+    asyncio.create_task(scheduler())
+
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
 
